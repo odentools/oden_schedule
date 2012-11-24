6d2461080c4082db48681be403109f8cb92d1af6
Masanori Ohgita <piyo3728@gmail.com>
2012-11-25 02:56:32 +0900

    Worker::Batch - Implement

--- site/lib/OdenSchedule/DBSchema.pm
+++ site/lib/OdenSchedule/DBSchema.pm
@@ -22,7 +22,7 @@ install_model user => schema {
 	index 'id';
 	index 'google_id';
 	index 'session_token';
-	columns qw/ name student_no type session_token google_id google_token google_reftoken calendar_id_gcal latest_auth_time latest_mail_id /;
+	columns qw/ name student_no type session_token google_id google_token google_reftoken calendar_id_gcal latest_batch_time latest_auth_time latest_mail_id /;
 	column 'user.id';
 };
 
--- site/lib/OdenSchedule/Worker/Batch.pm
+++ site/lib/OdenSchedule/Worker/Batch.pm
@@ -4,6 +4,10 @@ use strict;
 use warnings;
 use utf8;
 
+use OdenSchedule::Model::User;
+use OdenSchedule::Model::ScheduleCrawler;
+use OdenSchedule::Model::CalendarOrganizer;
+
 sub new {
 	my ($class, $app, $db) = @_;
 	my $self = bless({}, $class);
@@ -11,15 +15,53 @@ sub new {
 	$self->{app}= $$app;
 	$self->{db}	= $$db;
 	
+	$self->{batch_interval} = $self->{app}->config->{batch_interval} || die('Not specified batch_interval');
 	
 	$self->{app}->log->debug("-----Worker::Batch-----");
+	
+	$self->runBatch();
 	
 	return $self;
 }
 
-sub findBatch {
+sub runBatch {
 	my $self = shift;
 	
+	# Iterate all users from DB
+	my $iter = $self->{db}->get(user => 1);
+	while(my $row = $iter->next){
+		my $item = $row->{column_values};
+		
+		if( $item->{latest_batch_time} eq "" 
+			|| $item->{latest_batch_time} <= (time() -  $self->{batch_interval}) 
+		){# If time passes interval from the last processing ...
+			$self->{app}->log->debug("    * Run batch: user.id = ".$item->{id});
+		
+			my $user_object = OdenSchedule::Model::User->new( \($self->{db}), \($self->{app}->log), {'id'=>$item->{id}} );
+			$user_object->latest_batch_time(time());
+			$user_object->update();
+			
+			# 休講・補講情報の取得
+			my $crawler = OdenSchedule::Model::ScheduleCrawler->new(
+				'db' => \($self->{db}),
+				'own_user' => \$user_object,
+				'logger' => \($self->{app}->log),
+				'config' => \($self->{app}->config),
+			);
+			$crawler->upsertCrawlToDatabase();
+			
+			# カレンダーの登録＆更新
+			my $calorg = OdenSchedule::Model::CalendarOrganizer->new(
+				'db' => \($self->{db}),
+				'own_user' => \$user_object,
+				'logger' => \($self->{app}->log),
+				'config' => \($self->{app}->config),
+			);
+			$calorg->upsertDatabaseToCalendar();
+			
+			$self->{app}->log->debug("    * Complete batch: user.id = ".$item->{id});			
+		}
+	}
 }
 
 1;
