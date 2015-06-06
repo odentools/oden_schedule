85d6f484466d89c997357d73897a85da83584574
Masanori Ohgita <mp@ohgita.info>
2015-06-06 12:52:51 +0900

    Fix: 最新の補講メールに対応

--- site/lib/OdenSchedule/Model/ScheduleCrawler.pm
+++ site/lib/OdenSchedule/Model/ScheduleCrawler.pm
@@ -37,31 +37,31 @@ our $PERIOD_TIME_TABLE_NAWATE = {
 sub new {
 	my ($class, %hash) = @_;
 	my $self = bless({}, $class);
-	
+
 	$self->{db}	= ${$hash{db}} || die ('Not specified db instance.');# Database instance
 	$self->{own_user} = ${$hash{own_user}} || die ('Not specified own_user.');# Current user instance
 	$self->{logger} = ${$hash{logger}} || die ('Not specified logger.');# Log(Mojo::Log) instance
 	$self->{config} = ${$hash{config}} || die ('Not specified config.');# Config(Mojo::Plugin::Config) instance
-	
+
 	# OAuth tokens
 	$self->{oauth_access_token} = $self->{own_user}->{oecu_token} || die ('Not specified oauth_access_token.');
 	$self->{oauth_refresh_token} = $self->{own_user}->{oecu_reftoken} || die ('Not specified oauth_refresh_token.');
-	
+
 	# App configurations
 	$self->{api_key} = $self->{config}->{social_google_apikey};
 	$self->{consumer_key} = $self->{config}->{social_google_key};
 	$self->{consumer_secret} = $self->{config}->{social_google_secret};
-	
+
 	# Member variable
 	$self->{username} = $self->{own_user}->{student_no};
-	
+
 	return $self;
 }
 
 sub upsertCrawlToDatabase {
 	my ($self, $isRetry) = @_;
 	my $user_id = $self->{own_user}->{id};
-	
+
 	# Crawl schedules from OECU Mail
 	my @schedules;
 	eval{
@@ -74,7 +74,7 @@ sub upsertCrawlToDatabase {
 			return;
 		}
 	}
-	
+
 	# Insert events to Database
 	foreach my $item(@schedules){
 		my $hash_str;
@@ -83,40 +83,40 @@ sub upsertCrawlToDatabase {
 			$hash_str .= $_."_".$item->{$_};
 		}
 		my $hash_id = Digest::SHA1::sha1_base64(Encode::encode_utf8($hash_str));
-		
+
 		my $db_iter = $self->{db}->get(schedule => {where => ['hash_id' => $hash_id]});
 		if(!defined($db_iter->next)){ # NOT found on DB ... insert
 			# Set other values
 			$item->{hash_id} = $hash_id;
-			
+
 			# Insert to DB
 			my $itemRow = $self->{db}->set(schedule => $item);
 		}
 	}
-	
+
 }
 
 sub crawl {
 	my $self = shift;
 	$self->log_debug_("Schedule::Crawler - crawl()");
-	
+
 	my @mail_bodies = $self->getMails_();
-	
+
 	my @schedules = ();
 	foreach my $mail (@mail_bodies){
 		my $campus_name;
 		if($mail =~ /発信元：大阪電気通信大学.*(四條畷|寝屋川).*/m){
 			$campus_name = $1;
-			
+
 			my $campus_timetable;
 			if($campus_name eq "寝屋川"){
 				$campus_timetable = $PERIOD_TIME_TABLE_NEYAGAWA;
 			}elsif($campus_name eq "四條畷"){
 				$campus_timetable = $PERIOD_TIME_TABLE_NAWATE;
 			}
-			
+
 			$self->log_debug_("* mail\n   * campus_name = $campus_name");
-			
+
 			if($mail =~ /(\d+)月(\d+)日 (.+)曜 (\d+)時限 (.*)\((.*)\) は休講です。/m){
 				my $period = $4; $period =~ tr/０-９/0-9/;
 				my $time = $campus_timetable->{$period} || "";
@@ -135,21 +135,22 @@ sub crawl {
 				};
 				$self->log_debug_("    * 休講");
 				push(@schedules, $hash);
-			}elsif($mail =~ /以下の日程で (.*)\((.*)\) の補講を行います。(\r\n|\n\r|\n|\r)(\d+)月(\d+)日 (.+)曜 (\d+)時限 (\S+)/m){
-				my $period = $7; $period =~ tr/０-９/0-9/;
+			}elsif($mail =~ /以下の日程で、(.*)（(.*)）の補講を行います。(\r\n|\n\r|\n|\r)(\d+)\/(\d+)\/(\d+)\((.+)\) (\d+)時限 (\S+)/m){
+				my $period = $8; $period =~ tr/０-９/0-9/;
 				my $time = $campus_timetable->{$period} || "";
 				my $hash = {
 					'type' => '補講',
 					'subject' => $1,
 					'teacher' => $2,
-					'month' => $4,
-					'day' => $5,
-					'wday' => $6,
+					'year' => $4,
+					'month' => $5,
+					'day' => $6,
+					'wday' => $7,
 					'period' => $period,
 					'time' => $time,
-					'date' => $self->paramToTimePiece_($4, $5, $time),
+					'date' => $self->paramToTimePiece_($5, $6, $time),
 					'campus' => $campus_name,
-					'room' => $8,
+					'room' => $9,
 				};
 				$self->log_debug_("    * 補講");
 				push(@schedules, $hash);
@@ -177,7 +178,7 @@ sub getMails_ {
 		@dirs = $oecu->getFolders();
 		if(!$oecu->getIMAPObject()->select(Encode::encode('IMAP-UTF-7','[Gmail]/すべてのメール'))){
 			$self->log_debug_("    * Can't select [Gmail]/すべてのメール ");
-			die("Can't select [Gmail]/すべてのメール");	
+			die("Can't select [Gmail]/すべてのメール");
 		}
 	};
 	if($@){
@@ -195,15 +196,15 @@ sub getMails_ {
 		'FROM','dportsys@mc2.osakac.ac.jp',
 		'SENTSINCE',$oecu->getIMAPObject()->Rfc3501_date(time() - (60 * 60 * 24 * 31 * 2)) #条件: 2ヶ月前以降のメール
 	);
-	
+
 	my @mail_bodies;
-	
+
 	$self->log_debug_("    * Iterate messages... ");
 	foreach my $msg (@msgs) {
 		my $body = $oecu->getIMAPObject()->body_string($msg) or die "Could not body_string: ", $self->{imap}->LastError;
 		push(@mail_bodies, Encode::decode_utf8($body));
 	}
-	
+
 	$self->log_debug_("    * getMails_ complete. ");
 	return @mail_bodies;
 }
@@ -211,16 +212,16 @@ sub getMails_ {
 sub paramToTimePiece_ {
 	my ($self, $month, $day, $time) = @_;
 	# [Precondition!] Less than 5 months (+/-) from current month.
-	
+
 	$self->log_debug_("Schedule::Crawler - paramToTimePiece_( $month $day $time )");
-	
+
 	my $current_t = Time::Piece::localtime();
 	my $current_year = $current_t->year;
 	my $next_year = $current_year + 1;
 	my $before_year = $current_year - 1;
 	my $current_month = $current_t->mon;
 	my $year;
-	
+
 	if(abs($month - $current_month) <= 5){
 		$year = $current_year;
 	}else{ # before year
@@ -230,7 +231,7 @@ sub paramToTimePiece_ {
 			$year = $before_year;
 		}
 	}
-	
+
 	$self->log_debug_("        * $year $month $day $time");
 	return Time::Piece->strptime($year.'-'.$month.'-'.$day.' '.$time.':00+0900', '%Y-%m-%d %T%z');
 }
