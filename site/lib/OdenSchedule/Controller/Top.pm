fde5ab422b1535213dd3cffa94401bf145b2c052
Masanori Ohgita <mp@ohgita.info>
2015-06-06 12:45:14 +0900

    Fix: Mojoliciousの仕様変更に対応

--- site/lib/OdenSchedule/Controller/Top.pm
+++ site/lib/OdenSchedule/Controller/Top.pm
@@ -20,18 +20,18 @@ sub top_guest {
 
 sub top_user {
 	my $self = shift;
-	
+
 	if(defined($self->flash("message_info"))){
 		$self->stash("message_info", $self->flash("message_info"));
 	}
-	
+
 	if(defined($self->param('set_calendar_id'))){
 		$self->ownUser->calendar_id_gcal($self->param('set_calendar_id'));
 		$self->ownUser->update();
-		$self->render_json({status => 1, calendar_id => $self->param('set_calendar_id')});
+		$self->render(json => {status => 1, calendar_id => $self->param('set_calendar_id')});
 		return;
 	}
-	
+
 	# Event list
 	my @schedules;
 	my $iter = $self->db->get(schedule => {
@@ -47,7 +47,7 @@ sub top_user {
 	}
 	@schedules = sort { $a->{date} <=> $b->{date} } @schedules;
 	$self->stash('schedules', \@schedules);
-	
+
 	# Calendars list
 	my $calorg = OdenSchedule::Model::CalendarOrganizer->new(
 		'db' => \($self->app->db),
@@ -55,18 +55,18 @@ sub top_user {
 		'logger' => \($self->app->log),
 		'config' => \($self->config),
 	);
-	
+
 	$self->stash('calendars', []);
 	eval{
 		# Fetch the calendars list with oauth-account
 		my @calendars = $calorg->getCalendarList();
 		$self->stash('calendars', \@calendars);
-		
+
 		if($self->ownUser->{calendar_id_gcal} eq "" || defined($self->flash('calendar_reselect'))){
 			# Automatic re-select the calendar
 			$self->ownUser->calendar_id_gcal($calendars[0]->{id});
 			$self->ownUser->update();
-			
+
 		}else{
 			# Check selected-calendar is available
 			my $flg_found_calendar = 0;
@@ -80,13 +80,13 @@ sub top_user {
 			}
 		}
 	}; $self->stash('message_error', $@) if($@);
-	
+
 	$self->stash('selected_calendar_id', $self->ownUser->{calendar_id_gcal});
-	
+
 	$self->stash('isUser_google', 1);
-	
+
 	$self->stash('batch_nextdatestr', Time::Piece->new($self->app->config->{batch_interval} + $self->ownUser->{latest_batch_time})->strftime("%Y-%m-%d %H:%M"));
-	
+
 	$self->render();
 }
 
